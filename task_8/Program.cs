a9952b120b5ce217d84d6f47564f8129cb2eeda8
Evgeniy <evgeniybatsunov@gmail.com>
2023-02-01 23:50:29 +0300

    task_8

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -3,3 +3,20 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Write("Input your number N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current_num = 1;
+
+while (current_num <= num)  
+{
+    if (current_num %2==0)
+    {
+        Console.Write(current_num + " "); 
+        current_num++;
+    }
+    else
+    {
+        current_num++;
+    }
+}    
